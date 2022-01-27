23f92d6a1cd4ea9e5ecf889c8f194ce6db2b63f6
zeng shichang <sodesu99@gmail.com>
2022-01-27 13:47:07 +0900

    init

---
+++ src/test/java/com/zsc/lemongrass/LemongrassApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zsc.lemongrass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LemongrassApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
